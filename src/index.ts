047399eea5e29e83dfb311fbe0630dfadf450a01
Jarred Kenny <jarred.kenny@tracktile.io>
2022-11-10 20:21:15 -0400

    fix: export serverless

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { Type, Static } from "@sinclair/typebox";
 export { Controller } from "./controller";
 export { Service, combineServices } from "./service";
 export { generate } from "./generate";
+export { serverless } from "./serverless";
 export * from "./types";
